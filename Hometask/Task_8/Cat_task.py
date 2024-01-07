1c6100b3776240e5667ff2975e4b55d837f7f521
Vitalii <derunvt@ukr.net>
2024-01-07 15:04:58 +0200

    added: comments in 8 Hometask

--- Hometask/Task_8/Cat_task.py
+++ Hometask/Task_8/Cat_task.py
@@ -1,4 +1,5 @@
 def generate_cat_circle(cats: int = 100) -> list:
+    # Just generate cats circle list
     cat_circle = []
     for i in range(cats):
         cat_circle.append([i+1, 'hat_off'])
@@ -6,6 +7,7 @@ def generate_cat_circle(cats: int = 100) -> list:
 
 
 def cat_round_rolls(data: list) -> list:
+    # Check hat on or off
     cat, hat = data
     if hat == 'hat_off':
         return [cat, 'hat_on']
@@ -14,14 +16,15 @@ def cat_round_rolls(data: list) -> list:
 
 
 def start_circle(rounds: int = 100, cats: int = 100) -> list:
+    # Do main job)
     cat_circle = generate_cat_circle(cats)
     if rounds > cats:
         raise ValueError(f'You can`t run {rounds} rounds, you have {cats} cats! Please try again. '
                          f'Cats must be greater (or equal) than rounds. ')
     for i in range(rounds):
-        for j in cat_circle[i::i+1]:
+        for j in cat_circle[i::i+1]: # check hats status only for specific cats
             cat_circle[j[0]-1] = cat_round_rolls(j)
     return cat_circle
 
 
-print(start_circle(11, 10))
+print(start_circle(100, 100))
